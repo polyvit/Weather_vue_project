0c0f9c2573a0bc9d66657db6cf16c3c6b4f164bd
Polina Limonova <polina98_08@mail.ru>
2024-03-05 13:32:11 +0300

    Add out-in animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <Navigation />
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -10,4 +14,13 @@ import { RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: 600ms ease all;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
