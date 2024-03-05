df7fb8c2c9c05848bfe63ad3d0390462cbb8dcd5
Polina Limonova <polina98_08@mail.ru>
2024-03-05 10:49:38 +0300

    CityView layout

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,5 +1,99 @@
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center">
+    <div
+      v-if="route.query.preview"
+      class="text-center text-white p-4 bg-weather-secondary w-full"
+    >
+      <p>
+        You're currently previewing this city, click the "+" icon to start
+        tracking it
+      </p>
+    </div>
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString("ru-Ru", {
+            weekday: "short",
+            day: "2-digit",
+            month: "long",
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString("ru-Ru", {
+            timeStyle: "short",
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
+      <p>Feels like {{ Math.round(weatherData.current.feels_like) }}&deg;</p>
+      <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img
+        class="w-[150px] h-auto"
+        :src="`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+        alt="image"
+      />
+    </div>
+    <hr class="border-white border-opacity-10 border w-full" />
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Hourly weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+            class="flex flex-col gap-4 items-center"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString("ru-Ru", {
+                  hour: "numeric",
+                })
+              }}
+            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="`https://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              alt="image"
+            />
+            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="border-white border-opacity-10 border w-full" />
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 day forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("ru-Ru", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt="image"
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}&deg;</p>
+            <p>L: {{ Math.round(day.temp.min) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,6 +101,7 @@ import axios from "axios";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+
 const opeweatherApiKey = "6ea716c1d27aebee221e04d81ad335c6";
 
 const getWeatherData = async () => {
